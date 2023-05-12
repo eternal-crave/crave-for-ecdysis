291c73e4ce86a4db5a3d9e2f76e71ab20fbc6748
David Khudaverydan <134926510+eternal-crave@users.noreply.github.com>
2023-05-13 00:08:09 +0400

    refactor

---
+++ Assets/Scripts/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Views;
+
+public class EntryPoint : MonoBehaviour
+{
+    [SerializeField] private MainView mainView;
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        var presenter = new MainViewPresenter(new MainViewModel(), mainView);
+    }
+}
